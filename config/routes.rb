319b225a5728db73b28b429420bdf633a42a012e
shahid <shahidkhan@gmail.com>
2023-10-18 11:59:41 +0530

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
   post '/auth/login', to: 'authentication#login'
-  get '/*a', to: 'application#not_found'
-  root 'posts#index'
   resources :posts do
     collection do
       delete 'destroy_all_posts', to: 'posts#destroy_all_posts'
